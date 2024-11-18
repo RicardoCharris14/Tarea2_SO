ad1b55622f7c09be10704d638e8c2ab8a304ba76
agustin <llms.agustin@gmail.com>
2024-11-18 12:57:43 -0300

    fix: Agrega las funciones como producer() consumer() para generar cada uno, ademas agrega el archivo cabera de este 'consumer_producter'

---
+++ include/consumer_producter.h
@@ -0,0 +1,11 @@
+#ifndef CONSUMER_PRODUCTER_H
+#define CONSUMER_PRODUCTER_H
+
+#include "monitor.h"
+
+void consumer(monitor &monitor, int ID);
+void producter(monitor &monitor, int ID);
+
+
+
+#endif
