8af00648a185c766e17b387f2d1a070eccdbe8f8
agustin <llms.agustin@gmail.com>
2024-11-18 18:35:27 -0300

    Agrega el tiempo de espera para los consumidores

--- include/consumer_producter.h
+++ include/consumer_producter.h
@@ -2,6 +2,8 @@
 #define CONSUMER_PRODUCTER_H
 
 #include "monitor.h"
+#include <thread>
+
 
 void consumer(monitor &monitor, int ID);
 void producter(monitor &monitor, int ID);
--- src/consumer_producter.cpp
+++ src/consumer_producter.cpp
@@ -7,9 +7,19 @@ void producer(monitor &monitor, int ID){
         std::cout << "Productor " << ID << " produjo: " << (i + ID * 100) << "\n";
     }
 }
-void comsumer(monitor &monitor, int ID){
+void consumer(monitor &monitor, int ID, int time){
     for (int i = 0; i < 5; i++){
-        int item = monitor.consume();
+        int item;
+        try{
+           item = monitor.consume();
+           std::cout << "Consumidor " << ID << " consumió: " << item << "\n";
+
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        
         std::cout << "Consumidor " << ID << " consumió: " << item << "\n";
     }
     
