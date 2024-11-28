251368f168ddaff658dfb4a7ee0098e74eeeac27
bvillarroelr <benjamin.villarroel3@gmail.com>
2024-11-28 13:54:01 -0300

    feat: memoria_fisica.h creado y seteado. Falta implementacion en su archivo cpp

---
+++ direccionamiento virtual/memoria_fisica.h
@@ -0,0 +1,18 @@
+#ifndef MEM_FISICA_H
+#define MEM_FISICA_H
+#include "marco_pagina.h"
+#include <vector>
+
+class MemFisica {
+private:
+	int size;
+	std::vector<Marco*> marcosPagina;
+public:
+	MemFisica(int);
+	int getSize();
+	void setSize(int);
+	Marco obtenerMarcoDisp(); // idea: retornar marco disponible o null en caso contrario
+	bool agregarMarco(Marco*); // idea: ver cuantas entradas tiene el vector. Si es menor que size pushear.
+};
+
+#endif
